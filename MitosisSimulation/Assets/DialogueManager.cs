71b3d20b284efa5dd2e7546a9ec665a0699d9efc
EthanHicks1 <Cyanotrichite@gmail.com>
2018-02-11 17:01:12 -0500

    Dialogue
    Added dialogue manager to control text displayed on screen.

---
+++ MitosisSimulation/Assets/DialogueManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour {
+
+    public Text extra;
+    public Text dialogueText;
+
+    public Animator animator;
+
+    private Queue<string> sentences;
+
+	// Use this for initialization
+	void Start () {
+        sentences = new Queue<string>();
+	}
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        animator.SetBool("IsOpen", true);
+        //Debug.Log("Starting conversation with " + dialogue.name);
+
+        //Extra and unused
+        extra.text = dialogue.name;
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        //Debug.Log(sentence);
+
+        //Simply displays text
+        //dialogueText.text = sentence;
+
+        //Displays sentence char by char
+        StopAllCoroutines(); //makes sure we don't animate muitple sentences
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    //Used to display a sentence char by char
+    IEnumerator TypeSentence(string sentence)
+    {
+        dialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
+    }
+
+    void EndDialogue()
+    {
+        Debug.Log("End of conversation");
+        animator.SetBool("IsOpen", false);
+    }
+
+}
