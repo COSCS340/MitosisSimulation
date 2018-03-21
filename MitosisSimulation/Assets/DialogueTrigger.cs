ae819b82ddc7f2e85b1c7d0c4d71b8ee8a197a1e
Ethan Hicks <Cyanotrichite@gmail.com>
2018-03-21 15:02:45 -0400

    Updated from Laptop
    Added in something from my laptop to see if I can get the change on my desktop.

--- MitosisSimulation/Assets/DialogueTrigger.cs
+++ MitosisSimulation/Assets/DialogueTrigger.cs
@@ -9,5 +9,7 @@ public class DialogueTrigger : MonoBehaviour {
     public void TriggerDialogue()
     {
         FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+        //New Change from laptop
+        gameObject.SetActive(false);
     }
 }
