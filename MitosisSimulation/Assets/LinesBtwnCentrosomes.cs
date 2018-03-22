02c23b7d5336a515abb801335bd6a775a5f55802
Forrest <forrest.sable@gmail.com>
2018-03-22 18:46:11 -0400

    centrosomes

---
+++ MitosisSimulation/Assets/LinesBtwnCentrosomes.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinesBtwnCentrosomes : MonoBehaviour {
+
+	LineRenderer line;
+	public GameObject[] verts;
+	// Use this for initialization
+	void Start () {
+		line = gameObject.GetComponent<LineRenderer>();
+		line.positionCount = 6; 
+		line.numCornerVertices = 20;
+		line.startWidth = 0.05f;
+		line.endWidth = 0.05f;
+		int i = 0;
+		foreach (GameObject vert in verts ) {
+			line.SetPosition(i, vert.transform.position);
+			i++;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
