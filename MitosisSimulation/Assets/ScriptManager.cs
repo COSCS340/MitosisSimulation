e761153e72eb555ca842f2d010a71dfc9c78eaf2
Forrest <forrest.sable@gmail.com>
2018-03-25 16:03:50 -0400

    update
    all stages present.

--- MitosisSimulation/Assets/ScriptManager.cs
+++ MitosisSimulation/Assets/ScriptManager.cs
@@ -12,19 +12,29 @@ public class ScriptManager : MonoBehaviour {
 	public GameObject c2pos;
 	public GameObject c3pos;
 	public GameObject[] chromatids;
+	public GameObject envelope;
+	public GameObject[] chromosomes;
+	public GameObject[] chromopoints;
+	public GameObject[] daughterchromosomes;
+	public GameObject cell;
 
     public void InvokeFunction(int index)
     {
 		switch (index) {
-            case 1:
-                TestFunction();
-                break;
-            case 2:
+            case 3:
 				Step1();
-                TestFunction2();
                 break;
-			case 3:
+			case 4:
 				Step2();
+				break;
+			case 5:
+				Step3();
+				break;
+			case 6:
+				Step4();
+				break;
+			case 7:
+				Step5();
 				break;
 			default:
                 Debug.Log("Default");
@@ -37,12 +47,15 @@ public class ScriptManager : MonoBehaviour {
 		foreach ( GameObject spindle in spindles1 ) {
 			spindle.SetActive(true);
 		}
-	}
-
-	void Step2() {
 		nucleolus.SetActive(false);
 		foreach (GameObject chromatid in chromatids ) {
 			chromatid.SetActive(true);
+		}
+	}
+
+	void Step2() {
+		foreach (GameObject chromatid in chromatids ) {
+			chromatid.SetActive(false);
 		}
 		foreach (GameObject spindle in spindles1 ) {
 			spindle.SetActive(false);
@@ -50,7 +63,69 @@ public class ScriptManager : MonoBehaviour {
 		foreach (GameObject spindle in spindles2 ) {
 			spindle.SetActive(true);
 		}
+		foreach (GameObject chromo in chromosomes ) {
+			chromo.SetActive(true);
+		}
 		centreole2.transform.position = c3pos.transform.position;
+		envelope.SetActive(false);
+	}
+
+	void Step3() {
+		int i;
+		GameObject chromo;
+		GameObject point;
+		for ( i = 0; i < chromosomes.Length; i++ ) {
+			chromo = chromosomes[i];
+			point = chromopoints[i];
+			chromo.transform.position = point.transform.position; 
+		}
+
+	}
+
+	void Step4() {
+		foreach ( GameObject chromo in chromosomes ) {
+			chromo.SetActive(false);
+		}
+		foreach (GameObject spindle in spindles2 ) {
+			spindle.SetActive(false);
+		}
+		foreach (GameObject daughter in daughterchromosomes ) {
+			daughter.SetActive(true);
+		}
+		Vector3 newscale = cell.transform.localScale;
+		newscale.y = 12f;
+		cell.transform.localScale = newscale;
+
+	}
+
+	void Step5() {
+		Vector3 newscale = cell.transform.localScale;
+		newscale.y = 10f;
+		cell.transform.localScale = newscale;
+		envelope.SetActive(true);
+		foreach ( GameObject chromatid in chromatids ) {
+			chromatid.SetActive(true);
+		}
+		foreach (GameObject daughter in daughterchromosomes ) {
+			daughter.SetActive(false);
+		}
+		Vector3 centreole1pos = centreole1.transform.position;
+		centreole1pos.y += 5.5f;
+		centreole1.transform.position = centreole1pos;
+
+		Vector3 centreole2pos = centreole2.transform.position;
+		centreole2pos.y -= 5.4f;
+		centreole2.transform.position = centreole2pos;
+
+		GameObject newcell = Instantiate(cell);
+
+		Vector3 higherpos = cell.transform.position;
+		Vector3 lowerpos = cell.transform.position;
+		lowerpos.y -= 5.0f;
+		higherpos.y += 5.0f;
+		newcell.transform.position = higherpos;
+		cell.transform.position = lowerpos;
+
 	}
 
     void TestFunction()
