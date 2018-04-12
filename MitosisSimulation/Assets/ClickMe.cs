e897f73b1990d80d048b505eb9eae21d37f44a3f
EthanHicks1 <Cyanotrichite@gmail.com>
2018-04-12 11:52:34 -0400

    Update
    Added in raycast to detect hitting all objects. It selects the last one so we should now be able to implement clicking on different parts of the cell.

--- MitosisSimulation/Assets/ClickMe.cs
+++ MitosisSimulation/Assets/ClickMe.cs
@@ -8,19 +8,30 @@ public class ClickMe : MonoBehaviour {
 	void Update () {
         if (Input.GetMouseButtonDown(0))
         {
-            RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit[] hits;
+            hits = Physics.RaycastAll(Camera.main.ScreenPointToRay(Input.mousePosition), 100.0f);
 
-            if (Physics.Raycast(ray, out hit, 100.0f))
+            for (int i = 0; i < hits.Length; i++)
             {
-                if (hit.transform != null)
-                {
-                    PrintName(hit.transform.gameObject);
+                RaycastHit hit = hits[i];
+                //PrintName(hit.transform.gameObject);
+                if (i == hits.Length - 1) {
+                    print(hit.transform.gameObject.tag);
                 }
             }
+                /*RaycastHit hit;
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+                if (Physics.Raycast(ray, out hit, 100.0f))
+                {
+                    if (hit.transform != null)
+                    {
+                        PrintName(hit.transform.gameObject);
+                    }
+                }*/
         }
 	}
-
+    //Unused
     private void PrintName(GameObject obj)
     {
         print(obj.name);
